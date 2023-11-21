ee33b40ecae46b18d98a11dd0d3e028ea1af4d27
juliashna <shornikova_110503@mail.ru>
2023-11-21 21:31:25 +0500

    Лабораторна работа 3

--- tests/test.py
+++ tests/test.py
@@ -7,17 +7,17 @@ from src.lib import Calculator
 def test_Fibonacchi_correct():
     assert Fibonacchi(5) == [0, 1, 1, 2, 3]
 
-def test_Fibonacchi_incorrect():
-    assert Fibonacchi(5) == [1, 1, 2, 3, 5]
+# def test_Fibonacchi_incorrect():
+#     assert Fibonacchi(5) == [1, 1, 2, 3, 5]
 
 def test_Puzirec_correct():
     assert Bubble([4, 8, 1, 3, 9, 13, 0]) == [0, 1, 3, 4, 8, 9, 13]
 
-def test_Puzirec_incorrect():
-    assert Bubble([4, 8, 1, 3, 9, 13, 0]) == [4, 8, 3, 1, 9, 13, 0]
+# def test_Puzirec_incorrect():
+#     assert Bubble([4, 8, 1, 3, 9, 13, 0]) == [4, 8, 3, 1, 9, 13, 0]
 
 def test_Calc_correct():
     assert Calculator(13, 7, '+') == 20
 
-def test_Calc_incorrect():
-    assert Calculator(8, 2, '*') == 15
+# def test_Calc_incorrect():
+#     assert Calculator(8, 2, '*') == 15
